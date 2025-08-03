986a0e5ecbde10317199008139e9eedfb599fcf0
mr-selviraja <mr.selviraja@gmail.com>
2025-08-03 14:13:45 +0530

    Develop responsive navbar

--- src/script.js
+++ src/script.js
@@ -1,1 +1,13 @@
-console.log('JavaScript is working!');
+const btnNavToggle = document.getElementById("btn-nav-toggle");
+const navMenuEl = document.querySelector(".header__nav--menu");
+const btnNavToggleBar1 = document.getElementById("btn-nav-toggle-bar-1");
+const btnNavToggleBar2 = document.getElementById("btn-nav-toggle-bar-2");
+
+// Function to toggle nav menu
+const toggleNavMenu = () => {
+    navMenuEl.classList.toggle("active");
+    btnNavToggleBar1.classList.toggle("active");
+    btnNavToggleBar2.classList.toggle("active");
+}
+
+btnNavToggle.addEventListener("click", toggleNavMenu);
